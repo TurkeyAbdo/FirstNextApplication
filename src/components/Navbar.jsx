a1b281a6976f6bf548bcc7388a761406821b821c
TurkeyAbdo <abdulrhmanturkey123@gmail.com>
2025-03-21 19:30:57 +0200

    added suspence to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import NavbarItem from './NavbarItem'
-function Navbar() {
+import React, { Suspense } from 'react';
+import NavbarItem from './NavbarItem';
 
+function Navbar() {
     return ( 
-        <div className='bg-amber-100 p-4 flex justify-center gap-4 lg:text-lg '>
-            <NavbarItem title="Trending" param="fetchTrending"/>
-            <NavbarItem title="Top Rated" param="fetchTopRated"/>
+        <div className='bg-amber-100 p-4 flex justify-center gap-4 lg:text-lg'>
+            <Suspense fallback={<div>Loading...</div>}>
+                <NavbarItem title="Trending" param="fetchTrending"/>
+                <NavbarItem title="Top Rated" param="fetchTopRated"/>
+            </Suspense>
         </div>
-     );
+    );
 }
 
 export default Navbar;
