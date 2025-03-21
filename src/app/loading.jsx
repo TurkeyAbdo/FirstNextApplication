2ba4db0c19568afef0ba8fd4f3041dd5e278cab4
TurkeyAbdo <abdulrhmanturkey123@gmail.com>
2025-03-21 19:37:18 +0200

    chnage the path of image

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function loading() {
   return (
     <div className='flex justify-center'>
-        <img src='../../public/loading.svg'  alt='loading...' />
+        <img src='loading.svg'  alt='loading...' />
     </div>
   )
 }
